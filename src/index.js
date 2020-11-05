5056c09a01b8610e29c2c7c4bc20e40a7c7b4a7d
Stephanie Zacharias <stephanie@Stephanies-MacBook-Pro-4.local>
2020-11-04 16:21:22 -0800

    remove unnecessary comments

--- src/index.js
+++ src/index.js
@@ -3,7 +3,3 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 
 ReactDOM.render(<App />, document.querySelector('#root'));
-
-// Make a plan! -> simple todo list; just add stuff for now
-// I need: an input text bar, ul to store todos (component to store new todos), 
-// create new todos as li (component to create new todos from the input text bar)
